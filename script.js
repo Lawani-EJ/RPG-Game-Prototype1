566414b8c9ba974f2deeca3b0681bea9b737a9a8
elyonjohn <elyonjohn60@gmail.com>
2024-02-10 10:07:25 +0100

    Assigned variables

---
+++ script.js
@@ -0,0 +1,10 @@
+let xp = 0;
+let health =100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+//JavaScript interacts with the HTML using the Document Object Model, or DOM. 
+let button1 = document.querySelector("#button1");
