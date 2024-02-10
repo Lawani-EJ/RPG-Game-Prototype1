23da37f326a524bbf464fc71230689db2ad81ee5
elyonjohn <elyonjohn60@gmail.com>
2024-02-10 18:14:35 +0100

    Initialized buttons and added interaction

--- script.js
+++ script.js
@@ -7,4 +7,36 @@ let monsterHealth;
 let inventory = ["stick"];
 
 //JavaScript interacts with the HTML using the Document Object Model, or DOM. 
-let button1 = document.querySelector("#button1");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text    = document.querySelector("#text");
+const xpText  = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore () {
+    const button1 = document.querySelector("#button1");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button1.onclick = buyHealth;
+    button2.innerText = "Buy weapon (30 gold)";
+    button2.onclick = buyWeapon;
+    button3.innerText = "Go to town square";
+    button3.onclick = goTown;
+}
+
+function goCave () {
+    console.log("Going to cave.");
+}
+
+function fightDragon () {
+    console.log("Fighting dragon.");
+}
+
+//initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
