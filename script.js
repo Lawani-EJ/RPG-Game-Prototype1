2257e938b48426d864244fe6cb824a4dd6d709e2
elyonjohn <elyonjohn60@gmail.com>
2024-02-11 22:28:02 +0100

    Updated features in JS file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Declared variables
 let xp = 0;
 let health =100;
 let gold = 50;
@@ -17,6 +18,25 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [{
+    name: "town square", //added an object property name
+    "button text": ["Go to store", "Go to cave", "Fight dragon"] //added a button text object property with an empty string value
+    // then gave 3 string values
+}]; //Declared empty variable and added an empty object 
+
+
+//moved the function above the go store function and added the content from the goStore Function
+function goTown () {
+        const button1 = document.querySelector("#button1");
+    button1.innerText = "Go to store";  //updated button element
+    button1.onclick = goStore; //updated onclick property
+    button2.innerText = "Go to cave";  //updated button element
+    button2.onclick = goCave;//updated onclick property
+    button3.innerText = "Fight dragon"; //updated button element
+    button3.onclick = fightDragon;//updated onclick property
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";  // modified the display text
+}
+
 
 function goStore () {
     const button1 = document.querySelector("#button1");
@@ -26,6 +46,7 @@ function goStore () {
     button2.onclick = buyWeapon;
     button3.innerText = "Go to town square";
     button3.onclick = goTown;
+    text.innerText = "You enter the store";  // modified the display text
 }
 
 function goCave () {
@@ -36,7 +57,21 @@ function fightDragon () {
     console.log("Fighting dragon.");
 }
 
+//created 3 empty functions 
+function buyHealth () {
+
+}
+
+function buyWeapon () {
+    
+}
+
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+//created another empty function
+function update (location){
+
+}
