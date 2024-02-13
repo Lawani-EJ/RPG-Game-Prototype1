ba97535cd631dd4f97058281e08411baed3887f0
elyonjohn <elyonjohn60@gmail.com>
2024-02-13 22:21:33 +0100

    Updated buyWeapon function inJS file

--- script.js
+++ script.js
@@ -100,13 +100,28 @@ text.innerText = "You do not have enough gold to buy health.";
 }
 
 function buyWeapon() {
+    if (currentWeapon < weapons.length - 1){
+
   if (gold >= 30) {
     gold -= 30;
     currentWeapon++;
     goldText.innerText = gold;
     let newWeapon = weapons[currentWeapon].name;
     text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
   }
+
+  else{
+    text.innerText = "You do not have enough gold to buy a weapon.";
+  }
+
+     }
+     
+  else{
+    text.innerText = "You already have the most powerful weapon!"
+  }
+
 }
 
 function fightSlime(){  // These 2 functions will be used in my cave object
